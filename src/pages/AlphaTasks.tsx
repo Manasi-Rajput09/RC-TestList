803a9f5a69405223682cb159a689faf4ff9449ea
Manasi Rajput <rajputmanasi2002@gmail.com>
2026-04-01 03:05:59 -0400

    cleared

--- src/pages/AlphaTasks.tsx
+++ src/pages/AlphaTasks.tsx
@@ -201,7 +201,7 @@ export default function AlphaTasks({ appId }: { appId: number }) {
                                 </svg>
                              </button>
                           </div>
-                          
+                          {/* new change */}
                        </div>
                     ))
                  )}
